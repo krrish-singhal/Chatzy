13e6777b401851cb0b41c6bd294b9c67a9327b48
krrish <krrishsinghal62@gmail.com>
2025-03-21 21:24:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+const PORT=9000
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("user-message", (message) => {
+        // Broadcast the message to all other clients except the sender
+        socket.broadcast.emit("message", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+    return res.sendFile(path.resolve("./public/index.html"));
+});
+
+server.listen(9000, () => console.log(`Server started at port http://localhost:${PORT}`));
